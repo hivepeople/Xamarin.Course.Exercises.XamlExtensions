bf37a4552f4040cbc92dc918565e18a8b89dd774
bvi@hivepeople.dk <snb@hivepeople.dk>
2016-11-25 14:49:35 +0100

    Added live coding example with usage of x:Static

---
+++ Xamarin.Course.Exercises.XamlExtensions/Xamarin.Course.Exercises.XamlExtensions/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace Xamarin.Course.Exercises.XamlExtensions
+{
+    public partial class MainPage : ContentPage
+    {
+        // Just a simple user class to display in a list in the UI
+        public class User {
+            public string Name { get; set; }
+            public string PhoneNumber { get; set; }
+        }
+
+        // We expose test data via a static field, to make it easily accessible from XAML
+        private static readonly ObservableCollection<User> TestUsers = new ObservableCollection<User>
+        {
+            new User { Name = "Benny Bananhue", PhoneNumber = "10203040" },
+            new User { Name = "Johan Navneskifter", PhoneNumber = "30405060" },
+            new User { Name = "Tonny Tupé", PhoneNumber = "20202020" },
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
